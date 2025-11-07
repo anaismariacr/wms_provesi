a1e1154a62dda3335fc8c6d6c548007cb03b9f7c
JUANERNESTOPINTO <111015674+JUANERNESTOPINTO@users.noreply.github.com>
2025-11-06 22:21:52 -0500

    Add require_POST decorator to views.py

--- pedidos/views.py
+++ pedidos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Pedido
 from django.utils import timezone
+from django.views.decorators.http import require_POST
 
 def pedidos_pendientes(request):
     """
