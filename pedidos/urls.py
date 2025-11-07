bd986c6764c29a3197e5c8c33f5cea0aebf6f273
JUANERNESTOPINTO <111015674+JUANERNESTOPINTO@users.noreply.github.com>
2025-11-06 22:16:06 -0500

    Add URL path for updating order status

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("pendientes/", views.pedidos_pendientes, name="pedidos_pendientes"),
     path("todos/", views.pedidos_todos, name="todos_pedidos"),
     path("lista/", views.pedidos_lista, name="pedidos_lista"),
+    path('<str:pedido_id>/actualizar-estado/', 
+         views.actualizar_estado_pedido, 
+         name='actualizar_estado_pedido'),
 ]
