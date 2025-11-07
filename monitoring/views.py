a3837256b6b6ab80ac13c7aca371efc6061b6d19
JUANERNESTOPINTO <111015674+JUANERNESTOPINTO@users.noreply.github.com>
2025-11-06 21:35:52 -0500

    Change index view to render lista_pedidos.html

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'lista_pedidos.html')
 
 def healthCheck(request):
     return HttpResponse('ok')
