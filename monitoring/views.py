699274249b9766963c106de82ac5bd413743b706
JUANERNESTOPINTO <111015674+JUANERNESTOPINTO@users.noreply.github.com>
2025-11-06 21:40:16 -0500

    Update template path in index view

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return render(request, 'lista_pedidos.html')
+    return render(request, 'pedidos/lista_pedidos.html')
 
 def healthCheck(request):
     return HttpResponse('ok')
